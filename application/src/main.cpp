5070e76f6459b212f1fd2ae877f6de56b6db185f
root <root@LAPTOP-3VAOIRNV.localdomain>
2026-03-28 15:53:55 +0100

    total implementation of new precomputed scoring

--- application/src/main.cpp
+++ application/src/main.cpp
@@ -54,11 +54,18 @@ int main(int argc, char* argv[]) {
         }
       },
       in_format);
-
+  
+  // --- STRADA 1:  ARCHITETTURA PRE-CALCOLATA ---
+  
+  mudock::info(">> RUNNING: PRECOMPUTED JIT scoring architecture");
+  mudock::precomputed_genetic_adt_pipeline pipe{protein};
+  
+  // --- STRADA STANDARD: ARCHITETTURA ORIGINALE MUDOCK ---
   // compute all the ligands according to the input configuration
+  /*
   mudock::info("Virtual screening the ligands ...");
   mudock::genetic_adt_pipeline pipe{protein};
-
+  */
   auto output_queue = std::make_shared<mudock::safe_stack<mudock::static_molecule>>();
   {
     auto threadpool = mudock::threadpool();
